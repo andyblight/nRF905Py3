dba8427b9009fa6aec3eb8705d23c4f251650598
Andy Blight <andy.blight@gmail.com>
2019-09-05 21:29:41 +0100

    Added PWR_UP control to test program

--- pigpio-test/test_nrf905.py
+++ pigpio-test/test_nrf905.py
@@ -13,11 +13,17 @@ def reset_gpios(pi):
     for pin in gpios:
         pi.set_mode(pin, pigpio.INPUT)
 
+def set_gpio(pi, pin, state):
+    pi.set_mode(pin, pigpio.OUTPUT)
+    pi.write(pin, state)
+
+def set_pwr_up(pi, state):
+    PWR = 22
+    set_gpio(pi, PWR, state):
 
 def set_csn(pi, state):
     CSN = 8
-    pi.set_mode(CSN, pigpio.OUTPUT)
-    pi.write(CSN, state)
+    set_gpio(pi, CSN, state):
 
 
 def test_spi(pi):
@@ -35,7 +41,7 @@ def test_spi(pi):
     command = bytearray()
     # INSTRUCTION_R_TX_ADDRESS = 0b00100011
     command.append(0b00100011)
-    print("Read transmit address command", command.hex())
+    print("Read transmit address command, 0x", command.hex())
     # Set CSN low (selected)
     set_csn(pi, 0)
     # Transfer the data
@@ -43,7 +49,7 @@ def test_spi(pi):
     # Set CSN high (not selected)
     set_csn(pi, 1)
     # Print what we received
-    print(count, data.hex())
+    print("Received "count, "0x", data.hex())
     # Close the spi bus
     pi.spi_close(spi_h)
 
@@ -56,8 +62,10 @@ pi = pigpio.pi()
 
 if pi.connected:
     reset_gpios(pi)
+    set_pwr_up(pi, 1)
     test_spi(pi)
     print("Tests finished.")
+    set_pwr_up(pi, 0)
     reset_gpios(pi)
     # Disconnect from pigpoid
     pi.stop()
