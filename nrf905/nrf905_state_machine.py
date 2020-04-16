3bbe88cea7f5ff0ef8177eb2a3616374a936a69b
Andy Blight <andy.blight@gmail.com>
2020-04-16 21:19:59 +0100

    State machine now looks right

--- nrf905/nrf905_state_machine.py
+++ nrf905/nrf905_state_machine.py
@@ -25,17 +25,17 @@ class Nrf905StateMachine:
     """ A sub-class of Nrf905 that manages states and transitions.
     The state names were taken from the nRF905 datasheet.
     """
-    states = ['power_save', 'standby', {
+    states = ['sleep', 'standby', {
               'name': 'transmitting', 'initial': 'waiting',
               'children': ['waiting', 'sending']
               }, {
               'name': 'receiving', 'initial': 'listening',
-              'children': ['listening' 'receiving_data', 'received']
+              'children': ['listening', 'receiving_data', 'received']
               }]
 
     def __init__(self):
         self._machine = Machine(
-            model=self, states=self.states, initial='power_save')
+            model=self, states=self.states, initial='sleep')
         # Add transitions:          (trigger name, previous state, next state)
         # Transmitting
         self._machine.add_transition(
@@ -43,13 +43,13 @@ class Nrf905StateMachine:
         self._machine.add_transition(
             'no_carrier', 'transmitting_waiting', 'transmitting_sending')
         self._machine.add_transition(
-            'data_ready_tx', 'transmitting', 'standby')
+            'data_ready_tx', 'transmitting_sending', 'standby')
         # Receiving states
         self._machine.add_transition(
-            'address_match', 'receiving_carrier_busy',
+            'address_match', 'receiving_listening',
             'receiving_receiving_data')
         self._machine.add_transition(
-            'bad_crc', 'receiving_receiving_data', 'receiving_carrier_busy')
+            'bad_crc', 'receiving_receiving_data', 'receiving_listening')
         self._machine.add_transition(
             'data_ready_rx', 'receiving_receiving_data', 'receiving_received')
         self._machine.add_transition(
@@ -58,21 +58,21 @@ class Nrf905StateMachine:
             'received2standby', 'receiving_received', 'standby')
         # Enter/leave receiving
         self._machine.add_transition(
-            'receiver_enable', 'standby', 'receiving_listening')
+            'receiver_enable', 'standby', 'receiving')
         self._machine.add_transition(
             'receiver_disable', 'receiving', 'standby')
         # Power states
-        self._machine.add_transition('power_up', 'power_save', 'standby')
+        self._machine.add_transition('power_up', 'sleep', 'standby')
         # This must be last so that the * works.
         self._machine.add_transition(
             'power_down',
-            ['standy', 'transmitting', 'receiving'], 'power_save')
+            ['standy', 'transmitting', 'receiving'], 'sleep')
 
     def output_graph(self):
         """ Outputs a graph of the state machine showing all states and
         transitions.
         """
-        self._machine.get_graph().draw('nrf905-state-machine.png', prog='dot')
+        self._machine.get_graph().draw('nrf905-state-machine.jpg', prog='dot')
 
     def is_busy(self):
         """ Returns True when in a state that can be interrupted with no loss
@@ -81,15 +81,7 @@ class Nrf905StateMachine:
         busy = True
         state = self.state
         logger.debug("is_busy: " + state)
-        if (state == 'power_save' or state == 'standby'
+        if (state == 'sleep' or state == 'standby'
                 or state == 'receiving_listening'):
             busy = False
         return busy
-
-    def is_receiving(self):
-        receiving = True
-        state = self.state
-        logger.debug("is_receiving: " + state)
-        if state == 'power_down' or state == 'standby':
-            receiving = False
-        return receiving
